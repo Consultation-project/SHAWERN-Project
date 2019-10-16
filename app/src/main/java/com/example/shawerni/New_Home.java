b3367affed254c35504818eda1e7a725219dad37
waad <al-fahad-14188@hotmail.com>
2019-10-17 02:15:53 +0300

    تحديث كود رفع السيرة الذاتيه , بعد ما حلينا مشكلة رفع الصورة

---
+++ app/src/main/java/com/example/shawerni/New_Home.java
@@ -0,0 +1,15 @@
+package com.example.shawerni;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class New_Home  extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_home_idea);
+
+    }
+
+}
