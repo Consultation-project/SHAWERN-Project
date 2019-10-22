1b519eda737e76ced21c910a31a7df1ae5bb9913
lena900 <55808214+lena900@users.noreply.github.com>
2019-10-22 16:30:17 +0300

    اضفت صفحة المستشار اللي يشوفها اليورز + الفيو لست للمستشارين خلصتها

---
+++ app/src/main/java/com/example/shawerni/ConsultantNewVersion.java
@@ -0,0 +1,77 @@
+package com.example.shawerni;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class ConsultantNewVersion extends AppCompatActivity {
+   private static final String TAG = "Activity";
+
+    private ArrayList<String> nName = new ArrayList<>();
+    private ArrayList<ConModule> con1 = new ArrayList<>();
+    ConModule n =new ConModule();
+    FirebaseDatabase database;
+    DatabaseReference retreff ;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.concrecycle);
+        Log.d(TAG, "onCreate");
+        Ename();
+
+
+    }
+ private void Ename() {
+     database= FirebaseDatabase.getInstance();
+     retreff=database.getReference("Consultant Request");
+
+     retreff.addValueEventListener(new ValueEventListener() {
+         @Override
+         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+             for(DataSnapshot snapshot :dataSnapshot.getChildren()){
+                 ConModule Nm = snapshot.getValue(ConModule.class) ;
+                 String name = Nm.getName();
+                 nName.add(name);
+                 inRecycle ();
+             }
+         }
+
+         @Override
+         public void onCancelled(@NonNull DatabaseError databaseError) {
+
+         }
+     });
+
+
+
+    }
+
+    private void inRecycle (){
+        RecyclerView recyclerView= findViewById(R.id.recycler_view);
+        MyReclyecon myr = new MyReclyecon(nName,this);
+        recyclerView.setAdapter(myr);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/shawerni/MyReclyecon.java
@@ -0,0 +1,75 @@
+package com.example.shawerni;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class MyReclyecon extends RecyclerView.Adapter<MyReclyecon.ViewHolder> {
+
+    private static final String TAG= "RecycleView";
+    private ArrayList<String>  Name = new ArrayList<>();
+    private Context con;
+    //private ArrayList<ConModule> cons =new ArrayList<>();
+
+    public MyReclyecon(ArrayList<String> name, Context con) {
+        Name = name;
+        this.con = con;
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.conce_cy,parent,false);
+        ViewHolder holder= new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "OnBinViewHolder:called.");
+        //final ConModule cM = cons.get(position);
+        final String userName = Name.get(position);
+        holder.NameOfConsultatnt.setText(userName);
+        holder.paerntlyout.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (con,ForUser.class);
+                intent.putExtra("name",userName);
+                con.startActivity(intent);
+
+            }
+            });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return Name.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+       TextView NameOfConsultatnt ;
+       RelativeLayout paerntlyout ;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            NameOfConsultatnt = itemView.findViewById(R.id.Name2);
+            paerntlyout = itemView.findViewById(R.id.paernt);
+        }
+    }
+
+}
