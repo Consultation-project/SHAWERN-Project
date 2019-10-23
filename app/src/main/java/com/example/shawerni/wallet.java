f7bf288be9bac364170102b66074db218271264a
ABEER <ABEER.A.ALJARBOA>
2019-10-23 15:54:45 +0300

    last version

--- app/src/main/java/com/example/shawerni/wallet.java
+++ app/src/main/java/com/example/shawerni/wallet.java
@@ -13,14 +13,48 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 
 
+public class wallet extends Fragment {
 
-public class wallet extends Fragment  {
+    private static final String TAG = "Activity";
+
+    private View view;
+    private ArrayList<String> nName = new ArrayList<> ();
+    private ArrayList<ConModule> con1 = new ArrayList<> ();
 
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.payconsultationlist, container, false);
+
+
+        Log.d (TAG, "onCreate");
+
+        //  Ename();
+
+        int counter = 1;
+
+        nName.add ("Consultation " + counter);
+
+        inRecycle();
+
+        return view;
+    }
+
+
+    private void inRecycle() {
+        RecyclerView recyclerView = view.findViewById (R.id.recycler_view);
+        MyReclyecon myr = new MyReclyecon (nName, getContext ());
+        recyclerView.setAdapter (myr);
+        recyclerView.setLayoutManager (new LinearLayoutManager (getContext ()));
+
+
+    }
 }
